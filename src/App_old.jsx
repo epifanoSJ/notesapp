d3044a3e4a19ea81f6b66500f6bcffbec5da542b
Epifano San Jose <epifano.sanjose@gmail.com>
2026-01-23 19:33:05 +1100

    the notes app

--- src/App_old.jsx
+++ src/App_old.jsx
@@ -1,3 +1,17 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Hello from Infocentric EMIA Managed Services Team</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
+
 import { useState, useEffect } from "react";
 import {
   Authenticator,
